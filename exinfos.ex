48a275c1cdbb78c46251e97cc887e6b2dfdbf549
PCSOFT-WINDEV <appli@pcsoft.fr>
2023-11-21 19:14:20 +0100

    Initial Commit

---
+++ exinfos.ex
@@ -0,0 +1,10 @@
+[INFOS]
+gaf=0
+title=WW_Overview_Mobile_Application
+image=description.gif
+description=description.rtf
+source=1
+preview=preview.png
+typesite=0
+[OBJECT]
+WW_Overview_Mobile_Application.wwp=4096
